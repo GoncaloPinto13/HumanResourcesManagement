0a28de3be9e4a4a5f9c9070139faaad6a6a0be19
Diogo Rodrigues <diogo.rodrigues.t0130425@edu.atec.pt>
2025-09-12 22:56:09 +0100

    Atualizado - clients

--- HumanResources/Repositories/ClientRepository.cs
+++ HumanResources/Repositories/ClientRepository.cs
@@ -1,9 +1,10 @@
-﻿using HumanResources.Data;
+﻿using HumanResources.Data.Repositories;
 using HumanResources.Models;
+using HumanResources.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-namespace HumanResources.Repositories
-{/// Repositório de Client: usa a base e acrescenta queries típicas.
+namespace HumanResources.Data.Repositories
+{
     public class ClientRepository : GenericRepository<Client>, IClientRepository
     {
         public ClientRepository(Context context) : base(context) { }
@@ -19,5 +20,15 @@ namespace HumanResources.Repositories
                 .Where(c => c.CompanyName.StartsWith(prefix))
                 .OrderBy(c => c.CompanyName)
                 .ToListAsync();
+
+        public override async Task<Client> AddAsync(Client entity)
+        {
+            if (await ExistsAsync(c => c.Nif == entity.Nif))
+                throw new Exception($"Já existe um cliente com NIF {entity.Nif}");
+
+            return await base.AddAsync(entity);
+        }
+
+
     }
 }
--- HumanResources/Repositories/GenericRepository.cs
+++ HumanResources/Repositories/GenericRepository.cs
@@ -1,5 +1,7 @@
 ﻿using HumanResources.Data;
+using HumanResources.Models; // Ensure this using is present for Client
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace HumanResources.Repositories
 {
@@ -22,7 +24,7 @@ namespace HumanResources.Repositories
             public async Task<T?> GetByIdAsync(int id)
                 => await _db.FindAsync(id);                 // usa cache do contexto se existir
 
-            public async Task<T> AddAsync(T entity)
+            public virtual async Task<T> AddAsync(T entity)
             {
                 await _db.AddAsync(entity);
                 await _context.SaveChangesAsync();
@@ -47,4 +49,52 @@ namespace HumanResources.Repositories
                 => await _db.AnyAsync(predicate);
 
         }
+
+    public class ClientRepository : GenericRepository<Client>, IClientRepository
+    {
+        public ClientRepository(Context context) : base(context)
+        {
+        }
+
+        public override async Task<Client> AddAsync(Client entity)
+        {
+            var created = await _context.Set<Client>()
+                .FromSqlInterpolated(
+                    $"EXEC dbo.spClient_Create {entity.CompanyName}, {entity.Nif}, {entity.Email}")
+                .AsNoTracking()
+                .SingleAsync(); // deve vir 1 linha
+            return created;
+        }
+
+        public async Task<IEnumerable<Client>> GetClientsWithPendingTasksAsync()
+        {
+            // CS1061 fix: 'Client' does not contain a definition for 'Tasks'.
+            // You must either:
+            // 1. Add a 'Tasks' navigation property to the 'Client' class (if it exists in your model).
+            // 2. Or, if 'Tasks' are not related to 'Client', remove or replace this query.
+
+            // Option 1: If 'Tasks' should be a navigation property, add this to your Client class:
+            // public ICollection<Task> Tasks { get; set; }
+
+            // Option 2: If you do not have a 'Tasks' property, you need to clarify how to get pending tasks for clients.
+            // For now, comment out the code to avoid the error:
+            throw new NotImplementedException("Client does not have a 'Tasks' property. Please add it to the Client model or update this method.");
+        }
+
+        // Implement missing IGenericRepository<Client> members
+        public async Task<List<Client>> GetAllAsync()
+            => await base.GetAllAsync();
+
+        public async Task<Client?> GetByIdAsync(int id)
+            => await base.GetByIdAsync(id);
+
+        public async Task UpdateAsync(Client entity)
+            => await base.UpdateAsync(entity);
+
+        public async Task DeleteAsync(int id)
+            => await base.DeleteAsync(id);
+
+        public async Task<bool> ExistsAsync(Expression<Func<Client, bool>> predicate)
+            => await base.ExistsAsync(predicate);
+    }
 }
