e5a2ff69acfd4cbaf177c20b08056ccad65ece8f
Gonçalo Pinto <goncalopinto13032004@gmail.com>
2025-09-17 19:00:38 +0100

    login feito

--- HumanResources/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ HumanResources/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,19 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 using HumanResources.Areas.Identity.Data;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering; // Adicionado para SelectList
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HumanResources.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<HumanResourcesUser> _signInManager;
