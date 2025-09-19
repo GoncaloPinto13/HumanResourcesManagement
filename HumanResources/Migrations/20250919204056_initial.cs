ee0a1797b83dde582de5f021debb03e0839eb1ac
Helom Valentim <helom.valentim.t0127856@edu.atec.pt>
2025-09-19 21:54:43 +0100

    Correcao BUGS relatorio

--- HumanResources/Migrations/20250919204056_initial.cs
+++ HumanResources/Migrations/20250919204056_initial.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace HumanResources.Migrations
 {
     /// <inheritdoc />
-    public partial class teste123 : Migration
+    public partial class initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
